79caf2ae3168446480d757cd3057116fb8ce310f
Dmytro_Shedin <dmytro.shedin@pearson.com>
2022-05-16 00:15:03 +0300

    Added a new package

--- src/test/java/com/shedin/MainTest.java
+++ src/test/java/com/shedin/MainTest.java
@@ -6,8 +6,6 @@ import org.openqa.selenium.WebDriver;
 
 public class MainTest {
 	public static final String URL = "https://semantic-ui.com/modules/dropdown.html";
-	public static final String API_PATH = "public/v2/users";
-
 	public static void main(String[] args) {
 		WebDriver driver = DriverManager.getDriver();
 		driver.get(URL);
