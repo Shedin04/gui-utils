605dd0cae02b5eeed78734f60187695ad5c053eb
Dmytro_Shedin <dmytro.shedin@pearson.com>
2022-06-08 23:53:20 +0300

    refactoring

--- src/main/java/com/shedin/guicore/components/AbstractFragment.java
+++ src/main/java/com/shedin/guicore/components/AbstractFragment.java
@@ -2,24 +2,21 @@ package com.shedin.guicore.components;
 
 import com.codeborne.selenide.SelenideElement;
 import lombok.AccessLevel;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Selenide.$;
 
+
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public abstract class AbstractFragment {
+	@Setter
+	@Getter
+	private By rootElementLocator;
 	private SelenideElement rootElement;
-
 	private final By alert = By.cssSelector(".error-message-container.error");
-
-	protected SelenideElement getRootElement() {
-		return rootElement;
-	}
-
-	public void setRootElement(SelenideElement element) {
-		this.rootElement = element;
-	}
 
 	public boolean isDisplayed() {
 		return getRootElement().isDisplayed();
@@ -31,5 +28,12 @@ public abstract class AbstractFragment {
 
 	public String getAlertMessage() {
 		return $(alert).getText();
+	}
+
+	public SelenideElement getRootElement() {
+		if (rootElement == null) {
+			rootElement = $(getRootElementLocator());
+		}
+		return rootElement;
 	}
 }
